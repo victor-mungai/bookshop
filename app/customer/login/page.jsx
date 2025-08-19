8cbc22aeb6e09817aea6b5bd42e51bd20121976c
victor mungai <122648155+victor-mungai@users.noreply.github.com>
2025-08-19 18:42:57 +0400

    Update page.jsx

--- app/customer/login/page.jsx
+++ app/customer/login/page.jsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/customer/login.php`, {
+      const res = await fetch(`apache-php/bookshop/customer/login.php`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
