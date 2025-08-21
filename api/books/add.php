5e337228658d8d60ab4824e1c347da79c8672e86
victor mungai <122648155+victor-mungai@users.noreply.github.com>
2025-08-21 22:41:56 +0400

    Update add.php

--- api/books/add.php
+++ api/books/add.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-$mysqli = new mysqli("localhost", "root", "", "bookshop");
+$mysqli = new mysqli("mysql", "root", "1234", "bookshop");
 
 if ($mysqli->connect_error) {
   echo json_encode(['success' => false, 'message' => 'Database connection failed']);
