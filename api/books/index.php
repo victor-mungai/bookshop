46404b957d46b00a62ed4b13bef456eb34aac0d6
victor mungai <122648155+victor-mungai@users.noreply.github.com>
2025-08-21 22:42:15 +0400

    Update index.php

--- api/books/index.php
+++ api/books/index.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$conn = new mysqli("localhost", "root", "", "bookshop");
+$conn = new mysqli("mysql", "root", "1234", "bookshop");
 
 if ($conn->connect_error) {
   echo json_encode(["success" => false, "message" => "Database connection failed"]);
