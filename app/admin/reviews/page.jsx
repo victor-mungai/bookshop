d8ae8b6986a2482b3b4c34290721372db5c7de56
victor mungai <122648155+victor-mungai@users.noreply.github.com>
2025-08-21 22:15:16 +0400

    Update page.jsx

--- app/admin/reviews/page.jsx
+++ app/admin/reviews/page.jsx
@@ -6,7 +6,7 @@ export default function AdminReviewsPage() {
   const [error, setError] = useState('');
 
   useEffect(() => {
-    fetch('http:// process.env.server/bookshop/api/admin/fetch_reviews.php')
+    fetch('http://process.env.server/bookshop/api/admin/fetch_reviews.php')
       .then(res => res.json())
       .then(data => {
         if (data.success) setReviews(data.reviews);
