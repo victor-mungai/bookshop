93d91f3a329646f61aaff31ebdcc392fe2bc9bcb
victor mungai <122648155+victor-mungai@users.noreply.github.com>
2025-08-21 22:40:58 +0400

    Update update.php

--- api/admin/update.php
+++ api/admin/update.php
@@ -17,7 +17,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // DB connection
-$mysqli = new mysqli("localhost", "root", "", "bookshop");
+$mysqli = new mysqli("mysql", "root", "1234", "bookshop");
 if ($mysqli->connect_errno) {
   echo json_encode(["success" => false, "message" => "Database connection failed"]);
   exit();
