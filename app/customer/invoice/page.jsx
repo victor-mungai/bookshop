7f2ff64b9aaaa003714a4533942b09799e1bc4b2
victor mungai <122648155+victor-mungai@users.noreply.github.com>
2025-08-28 16:13:42 +0400

    Update page.jsx

--- app/customer/invoice/page.jsx
+++ app/customer/invoice/page.jsx
@@ -27,7 +27,7 @@ export default function InvoicePage() {
       return;
     }
 
-    fetch(`http://process.env.server/bookshop/api/invoice.php?order_id=${orderId}`)
+    fetch(`/bookshop/api/invoice.php?order_id=${orderId}`)
       .then(res => res.json())
       .then(data => {
         if (data.success) {
